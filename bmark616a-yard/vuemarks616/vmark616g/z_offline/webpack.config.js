c3082169cdbd9987efe3571525260829be6f6248
albe <albe@host211220-1121.enwebhost.net>
2022-01-02 16:48:11 -0500

    2022-01-02d

---
+++ bmark616a-yard/vuemarks616/vmark616g/z_offline/webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack')
+
+    devServer: {
+      host: '0.0.0.0',//your ip address
+      port: 8081,
+      disableHostCheck: true
+  }
+
+})
+
+module.exports = webpackConfig
