8560be8628357d2255327c59e9c8e8bfb3befbb0
albe <albe@host211220-1121.enwebhost.net>
2022-01-07 12:54:10 -0500

    2022-01-07a

--- bmark616a-yard/vuemarks616/vmark616g/shi.sh
+++ bmark616a-yard/vuemarks616/vmark616g/shi.sh
@@ -9,8 +9,12 @@ source ./.env
 
 # install..
 
-docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c " npm install --save /vmark616f1/198.23.238.244:8080
-"
-docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c "npm i"
+#docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c " npm install --save /vmark616f1/198.23.238.244:8080"
+
+#docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c "npm i --save @vue/cli"
+
+docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c "vue add pwa"
+
+#docker run --rm -v "${PWD}:/$(basename `pwd`)" -w "/$(basename `pwd`)" -p $devport:8080 -it  $nodeimg sh -c "npm i"
 
 
